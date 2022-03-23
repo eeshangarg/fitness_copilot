15fdf14df8cf91fc052fd30eac06170c6c00b9e9
Eeshan Garg <eeshangarg@protonmail.com>
2022-03-23 00:49:45 -0400

    models: Add ExerciseSet model.
    This model represents a set done for a specified number of reps
    with a given amount of weight.

---
+++ lib/models/exercise_set.dart
@@ -0,0 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'exercise_set.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class ExerciseSet {
+  int numberOfReps = 0;
+  double weight = 0.0; // in lbs
+
+  ExerciseSet({required this.numberOfReps, required this.weight});
+
+  factory ExerciseSet.fromJson(Map<String, dynamic> json) =>
+      _$ExerciseSetFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ExerciseSetToJson(this);
+}
